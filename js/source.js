a5059cae02296a601140fa66906c6e55e137b3c5
tayler <tayler@rightintel.com>
2016-04-06 21:42:59 -0600

    use css to handle nav menus instead of js; fix home page responsive

--- js/source.js
+++ js/source.js
@@ -1,14 +1,14 @@
 (function() {
     'use strict';
     
-    var currentLoc = document.location.href;
-    var nav_links = Array.prototype.slice.call(document.querySelectorAll('.nav-links'));
-
-    nav_links.forEach(function(linkEl) {
-        if (currentLoc === linkEl.href) {
-console.log(currentLoc);
-            linkEl.style.cursor = "default";
-            linkEl.style.textDecoration = "none";
-        }
-    });
+    // ended up doing this with css instead
+    // var currentLoc = document.location.href;
+    // var nav_links = Array.prototype.slice.call(document.querySelectorAll('.nav-links'));
+    // 
+    // nav_links.forEach(function(linkEl) {
+    //     if (currentLoc === linkEl.href) {
+    //         linkEl.style.cursor = "default";
+    //         linkEl.style.textDecoration = "none";
+    //     }
+    // });
 })();
